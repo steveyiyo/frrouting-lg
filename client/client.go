db668b2cb869762c28478a3b4a025a37f89e8867
SteveYi <jackcooku@steveyi.net>
2021-01-04 19:12:26 +0800

    Update client.go

--- client/client.go
+++ client/client.go
@@ -2,40 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
-
-	"github.com/valyala/fasthttp"
 )
 
 var (
-	w        http.ResponseWriter
-	r        *http.Request
-	ctx      *fasthttp.RequestCtx
 	SourceIP = "1.1.1.1"
-	// addr     = flag.String("addr", ":8080", "TCP address to listen to")
-	// compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
 )
 
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	router := string(ctx.FormValue("router"))
-	action := string(ctx.FormValue("action"))
-	IP := string(ctx.FormValue("IP"))
-	fmt.Println(router, action, IP)
+func lg(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`FRRouting Looking Glass\n`))
+
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+
+	} else {
+		// 判斷
+		action := r.Form["action"][0]
+		IP := r.Form["IP"][0]
+		if action == "ping" {
+			ping(IP)
+			// fmt.Fprintf(w, "Unknow")
+		}
+		if action == "traceroute" {
+			traceroute(IP)
+			fmt.Fprintf(w, "Unknow")
+		}
+		if action == "mtr" {
+			mtr(IP)
+			fmt.Fprintf(w, "Unknow")
+		}
+		if action == "bgpsummary" {
+			bgpsummary()
+			fmt.Fprintf(w, "Unknow")
+		}
+		if action == "routev4" {
+			routev4(IP)
+			fmt.Fprintf(w, "Unknow")
+		}
+		if action == "routev6" {
+			routev6(IP)
+			fmt.Fprintf(w, "Unknow")
+		}
+	}
 }
 
-func ping() {
+func ping(IP string) {
 	fmt.Print("Client has been start!\n")
 
 	cmd := exec.Command("bash", "-c", "ping -I", SourceIP, "-O -c 10", IP)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	fmt.Println(string(out))
 }
 
-func traceroute() {
+func traceroute(IP string) string {
 	fmt.Print("Client has been start!\n")
 
 	cmd := exec.Command("bash", "-c", "traceroute", IP, "-a", SourceIP)
@@ -44,9 +72,10 @@ func traceroute() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(out))
+	return string(out)
 }
 
-func mtr() {
+func mtr(IP string) {
 	fmt.Print("Client has been start!\n")
 
 	cmd := exec.Command("bash", "-c", "mtr -G 2 -c 10 -erwbz", IP, "--address", SourceIP)
@@ -55,6 +84,7 @@ func mtr() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(out))
+	return
 }
 
 func bgpsummary() {
@@ -66,9 +96,10 @@ func bgpsummary() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(out))
+	return
 }
 
-func routev4() {
+func routev4(IP string) {
 	fmt.Print("Client has been start!\n")
 
 	cmd := exec.Command("bash", "-c", "vtysh -c 'show ip bgp $ip'")
@@ -77,9 +108,10 @@ func routev4() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(out))
+	return
 }
 
-func routev6() {
+func routev6(IP string) {
 	fmt.Print("Client has been start!\n")
 
 	cmd := exec.Command("bash", "-c", "vtysh -c 'show bgp $ip'")
@@ -88,4 +120,14 @@ func routev6() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(out))
+	return
+}
+
+func main() {
+	http.HandleFunc("/", lg)
+
+	err := http.ListenAndServe(":32991", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
