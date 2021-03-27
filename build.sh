d09a36709b4d598f40845e8beeea106922bf29a0
SteveYi <jackcooku@steveyi.net>
2021-03-27 22:28:16 +0800

    New Version for Server

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting for build!"
+
+echo "Linux amd64"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/Linux_Intel64
+echo "macOS amd64"
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/macOS_Intel64
+echo "Windows amd64"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/Windows_Intel64.exe
--- server.go
+++ server.go
@@ -3,12 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
-	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 type router []struct {
@@ -17,105 +16,55 @@ type router []struct {
 	IP   string `json:"ip"`
 }
 
-func lg(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// io.WriteString(w, "https://github.com/steveyiyo/frrouting-lg")
-
-	r.ParseForm()
-	fmt.Println("\nIP Address: " + getIP(r))
-	fmt.Println("method:", r.Method)
-	fmt.Println(r.URL.Path)
-	p := "." + r.URL.Path
-
-	exist := "NULL"
-	if _, err := os.Stat(p); err == nil {
-		exist = "1"
-	} else if os.IsNotExist(err) {
-		exist = "0"
-	}
-
-	if r.Method == "GET" {
-		if exist == "1" {
-			if p == "./" {
-				p = "./v1.html"
-			}
-			if p == "./v1" {
-				p = "./v1.html"
-			}
-			if p == "./v2" {
-				p = "./v2.html"
-			}
-			http.ServeFile(w, r, p)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			c, err := ioutil.ReadFile("./404.html")
-			if err != nil {
-				fmt.Println(err)
-			}
-			w.Write(c)
-		}
-	} else if r.Method == "POST" {
-		err := r.ParseMultipartForm(5 * 1024 * 1024 * 1024)
-		if err != nil {
-			fmt.Println("Error ParseMultipartForm: ", err)
-			return
-		}
-
-		data, err := ioutil.ReadFile("router.json")
-
-		var router router
-		json.Unmarshal(data, &router)
-
-		// fmt.Println(router)
-
-		Router := "NULL"
-		Action := "NULL"
-		IP := "NULL"
-
-		Router = r.FormValue("Router")
-		Action = r.FormValue("Action")
-		IP = r.FormValue("IP")
-
-		fmt.Println("Action: ", Action)
-		fmt.Println("IP: ", IP)
-		fmt.Println("Router: ", Router)
-
-		var RouterIP string
-		RouterIP = "NULL"
-		for n := range router {
-			if router[n].Name == Router {
-				fmt.Println("Router IP: ", router[n].IP)
-				RouterIP = router[n].IP
-			}
-		}
-
-		RouterURL := "http://" + RouterIP + ":32991"
-		resp, err := http.PostForm(RouterURL,
-			url.Values{"Action": {Action}, "IP": {IP}})
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			// handle error
-		}
-		fmt.Println(string(body))
-		io.WriteString(w, string(body))
+func indexPage(c *gin.Context) {
+	if c.Param("version") == "v1" {
+		c.HTML(200, "v1.tmpl", nil)
+	} else if c.Param("version") == "" {
+		c.HTML(200, "v2.tmpl", nil)
+	} else {
+		pageNotAvailable(c)
 	}
 }
 
-func getIP(r *http.Request) string {
-	forwarded := r.Header.Get("X-FORWARDED-FOR")
-	if forwarded != "" {
-		return forwarded
+func LookingGlass(c *gin.Context) {
+	Router := c.PostForm("Router")
+	Action := c.PostForm("Action")
+	IP := c.PostForm("IP")
+
+	data, err := ioutil.ReadFile("router.json")
+
+	var router router
+	json.Unmarshal(data, &router)
+
+	var RouterIP string
+	RouterIP = "NULL"
+	for n := range router {
+		if router[n].Name == Router {
+			fmt.Println("Router IP: ", router[n].IP)
+			RouterIP = router[n].IP
+		}
 	}
-	return r.RemoteAddr
+
+	RouterURL := "http://" + RouterIP + ":32991"
+	resp, err := http.PostForm(RouterURL,
+		url.Values{"Action": {Action}, "IP": {IP}})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+	c.String(200, string(body))
+}
+
+func pageNotAvailable(c *gin.Context) {
+	c.HTML(404, "404.tmpl", nil)
 }
 
 func main() {
-	http.HandleFunc("/", lg)
 
 	fmt.Print("\n")
 	fmt.Print("-------------------\n")
@@ -129,8 +78,16 @@ func main() {
 	fmt.Print("-------------------\n")
 	fmt.Print("\n")
 
-	err := http.ListenAndServe(":32280", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	router := gin.New()
+	router.Use(gin.Logger(), gin.Recovery())
+	router.LoadHTMLGlob("static/*")
+
+	router.GET("/", indexPage)
+	router.GET("/:version", indexPage)
+	router.POST("/", LookingGlass)
+
+	router.NoRoute(pageNotAvailable)
+
+	router.Run("127.0.0.1:32280")
+
 }
